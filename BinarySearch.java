137a2daf6091a15aaa1ff4588e3de0ad08987aa3
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-10-22 21:07:45 +0530

    Day-15

--- BinarySearch.java
+++ BinarySearch.java
@@ -43,12 +43,49 @@ public class BinarySearch {
     static int findTargetElementRotatedArray(int[] arr, int target){
        int start = 0;
        int end = arr.length-1;
-       int ans = -1;
        while (start <= end) { 
         int mid = start + (end - start)/2;
-        
+     //first we have to check the rotated array in which part of array is sorted
+     //left or right
+     if (arr[mid] == target) {
+        return mid;
+     }
+        else if(arr[start] <= arr[mid]) {
+            if (target > arr[start] && target <= arr[mid]) {
+                end = mid -1;
+            }else{
+                start = mid + 1;
+            }
+        }else{
+            if (target > arr[mid] && target <= arr[end]) {
+                start = mid +1;
+            }else{
+                end = mid - 1;
+            }
+        }
     }
-    return ans;
+// this is also same but reverse check 
+    /* while (start <= end) {
+        int mid = start + (end - start) / 2;
+        // first we have to check the rotated array in which part of array is sorted
+        // left or right
+        if (arr[mid] == target) {
+            return  mid;
+        } else if (arr[start] <= arr[mid]) {
+            if (arr[start] <= target && target <= arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (arr[mid] <= target && target <= arr[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    } */
+    return -1;
     }
     public static void main(String[] args) {
         // binary serch is only used in sorted array

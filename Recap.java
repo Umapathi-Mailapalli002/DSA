9d83a9f9b2c32e5abac5172623ba46748e19eccc
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-03-13 11:09:29 +0530

    add Recap class with decimal to binary conversion method skeleton

---
+++ Recap.java
@@ -0,0 +1,11 @@
+public class Recap {
+    static int decimal2binary(int num) {
+
+        while (num > 0) { 
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}

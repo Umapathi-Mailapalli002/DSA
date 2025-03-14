f33e88dbba5205b3d51ec1e95171fec58810cf4b
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-03-14 12:47:32 +0530

    add binary to decimal conversion method in Recap class

--- Recap.java
+++ Recap.java
@@ -10,8 +10,19 @@ public class Recap {
         }
         return n;
     }
-    
+    static  int binary2Decimal(int num){
+        int decimalNum = 0;
+        int pw = 1;
+        while (num > 0) { 
+            int lastDigit = num % 10;
+            decimalNum += lastDigit * pw;
+            num/=10;
+            pw*=2;
+        }
+        return  decimalNum;
+    }
     public static void main(String[] args) {
         System.out.println(decimal2binary(10));
+        System.out.println(binary2Decimal(1010));
     }
 }

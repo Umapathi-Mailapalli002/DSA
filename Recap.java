58f9374ca38dc27ca813779060b1e130f015330e
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-03-14 12:40:56 +0530

    implement decimal to binary conversion in Recap class

--- Recap.java
+++ Recap.java
@@ -1,11 +1,17 @@
 public class Recap {
     static int decimal2binary(int num) {
-
+        int n = 0;
+        int pw = 1;
         while (num > 0) { 
-            
+            int lastDigit = num%2;
+            n += lastDigit * pw;
+            num /= 2;
+            pw*=10;
         }
+        return n;
     }
+    
     public static void main(String[] args) {
-        
+        System.out.println(decimal2binary(10));
     }
 }

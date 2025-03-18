4c4151dbaf63d812c82fee9027df9ec3dd8745b6
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-03-18 09:33:12 +0530

    add method to find target element in rotated array in Recap class

--- Recap.java
+++ Recap.java
@@ -40,7 +40,29 @@ public class Recap {
         return  ans;
     }
 
-    
+    static int findTargetElementRotatedArray(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] == target) {
+                return  mid;
+            }else if (target <= arr[mid] ) {
+                if (target > arr[start] && target <= arr[mid]) {
+                    end = mid -1;
+                }else{
+                    start = mid + 1;
+                }
+            }else{
+                if (target > arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
+                }else{
+                    end = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         System.out.println(decimal2binary(10));
         System.out.println(binary2Decimal(1010));
